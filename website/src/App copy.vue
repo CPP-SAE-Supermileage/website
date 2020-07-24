ccc7a0c68f10409a263a6ad8873eddd10cf90fd6
Maximum Wilder-Smith <maxwildersmith@gmail.com>
2020-07-24 02:42:38 -0700

    Added new files for the website (#1)
    Co-authored-by: tempest124 <danielgruhn1998@gmail.com>

---
+++ website/src/App copy.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Slider />
+  </div>
+</template>
+
+<script>
+import Slider from "./components/Slider.vue";
+export default {
+  name: "app",
+  components: {
+    Slider
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+body {
+  margin: 0px;
+}
+</style>
