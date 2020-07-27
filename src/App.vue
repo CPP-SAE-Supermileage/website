cdfae06da1847c7b61ef0480a1defcd29cf69a41
The-Mills <mosonkahni@gmail.com>
2020-07-27 13:26:42 -0700

    Fixes and renames Image.vue. Edits to App.vue (#2)
    * Fixes and renames Image.vue. Edits to App.vue
    
    Vue does not like when the custom template component is named "Image". Hence, everything is now "ImageDisplayer"
    App.vue now preloads all images using require(), instead of relying on other components to load them from URL strings.
    Made the code in these two files better-structured and easier to read.
    
    * Fixes spacing and adds example for displayHeight
    
    V-parallax height defaults to 500. Added a comment and an extra example to clarify this.
    
    Co-authored-by: Daniel Milligan <ddmilligan@cpp.edu>

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,51 @@
 <template>
   <v-app>
     <Menu/>
-    <v-main>
-      
-      <Slideshow :imgs="slideImgs"/>
-  
+    <v-main> 
+      <Slideshow :imgs="[logo, carRender, window, profile, fiberGlass]"/> 
       <About/>
-      <Image :imgSrc="'@/assets/CarRendering.png'"/>
+      <ImageDisplayer :imgSrc="profile" :displayHeight="300" headerTxt="Header Text" txt="Other Text"/>
+      <br>
+      <ImageDisplayer :imgSrc="window" headerTxt="No displayHeight set" txt="Defaults to 500"/>
       <Competitions/>
       <Donate/>
       <Contact/>
-
     </v-main>
   </v-app>
 </template>
 
 <script>
-import Menu from './components/Menu';
-import Slideshow from './components/Slideshow';
-import About from'./components/About';
-import Competitions from './components/Competitions';
-import Donate from './components/Donate';
-import Contact from './components/Contact';
-import Image from './components/Image';
+	import Menu           from './components/Menu';
+	import Slideshow      from './components/Slideshow';
+	import About          from './components/About';
+	import Competitions   from './components/Competitions';
+	import Donate         from './components/Donate';
+	import Contact        from './components/Contact';
+	import ImageDisplayer from './components/ImageDisplayer';
 
-export default {
-  name: 'App',
+	export default 
+	{
+		name: 'App',
 
-  components: {
-    Menu,
-    Slideshow,
-    About,
-    Competitions,
-    Donate,
-    Contact,
-    Image,
-  },
+		components: 
+		{
+			Menu,
+			Slideshow,
+			About,
+			Competitions,
+			Donate,
+			Contact,
+			ImageDisplayer,
+		},
 
-  data: () => ({
-    slideImgs:[
-      require('@/assets/logo.png'),
-      require('@/assets/CarRendering.png')
-    ]
-  }),
-};
+		data: () => 
+		({
+			carRender:       require('@/assets/CarRendering.png'),
+			fiberGlass:      require('@/assets/Fiberglass.png'),
+			fiberGlassTwo:   require('@/assets/Fiberglass2.png'),
+			logo:            require('@/assets/logo.png'),
+			profile:         require('@/assets/Profile.png'),
+			window:          require('@/assets/Window.png'),
+		}),
+	};
 </script>
---
+++ src/components/ImageDisplayer.vue
@@ -0,0 +1,29 @@
+<template>
+  <!-- height is optional and defaults to 500-->
+  
+  <v-parallax :src="imgSrc" :height="displayHeight">
+	<v-row v-if="headerTxt||txt" align="center" justify="center">
+        <v-col class="text-center" cols="12">
+          <h2 v-if="headerTxt">{{headerTxt}}</h2>
+          <p v-if="txt">{{txt}}</p>
+        </v-col>
+    </v-row>
+  </v-parallax>
+</template>
+
+<script>
+	export default 
+	{
+		name: "ImageDisplayer",
+		props: 
+		{	
+			imgSrc:        String,
+			displayHeight: Number,
+			headerTxt:     String,
+			txt:           String,
+		},
+	};
+</script>
+
+<style scoped>
+</style>
