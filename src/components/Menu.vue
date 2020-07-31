b585670d8e7ca752a0f465da1ffd9ad7f45f3068
The-Mills <mosonkahni@gmail.com>
2020-07-30 18:58:12 -0700

    Makes menu bar mobile-friendly. (#11)
    Makes the menu bar mobile-friendly by collapsing the menu buttons into a hamburger that displays them in a navigation-drawer upon clicking. This occurs if the window width is below or reduced below a certain threshold.
    
    Co-authored-by: Daniel Milligan <ddmilligan@cpp.edu>

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,69 +1,92 @@
 <template>
-  <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="CPP Logo"
-          class="shrink mr-2 logo"
-          contain
-          :src="require('@/assets/logo.png')"
-        />
+<v-card>
 
-      </div>
+<v-app-bar  ref="appBarRef"  app  color="primary"  dark>
+    <div>
+        <v-img  alt="CPP Logo"  class="shrink mr-2 logo"  contain  :src="require('@/assets/logo.png')"/>
+    </div>
 
-      <v-spacer></v-spacer>
+    <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://cpp.thankyou4caring.org/pages/studentclub"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">About</span>
+    <div v-if="displayHamburger">
+        <v-app-bar-nav-icon  @click.stop="drawer=!drawer"></v-app-bar-nav-icon>
+    </div>
+
+    <div v-else> 
+        <v-btn  href="https://cpp.thankyou4caring.org/pages/studentclub"  target="_blank" text>
+            <span>About</span>     
+        </v-btn>
+
+        <v-btn  href="https://cpp.thankyou4caring.org/pages/studentclub"  target="_blank"  text>
+            <span>Competitions</span>       
+        </v-btn>
+
+        <v-btn  href="https://cpp.thankyou4caring.org/pages/studentclub"  target="_blank"  text>
+            <span>Donate</span>
+        </v-btn>
+      
+        <v-btn  href="https://cpp.thankyou4caring.org/pages/studentclub"  target="_blank"  text>
+            <span>Contact</span>  
+        </v-btn>
+    </div>     
+</v-app-bar>
+
+<v-navigation-drawer  v-model="drawer"  v-if="displayHamburger"  color="primary"  width="160"  app  right  temporary  dark>
+        <br><br>
+        <v-btn  href="https://cpp.thankyou4caring.org/pages/studentclub"  target="_blank"  text>
+            <span>About</span>       
+        </v-btn>
+        <br><br>
         
-      </v-btn>
-
-      <v-btn
-        href="https://cpp.thankyou4caring.org/pages/studentclub"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Competitions</span>
+        <v-btn  href="https://cpp.thankyou4caring.org/pages/studentclub"  target="_blank"  text>
+            <span>Competitions</span>       
+        </v-btn>
+        <br><br>
         
-      </v-btn>
-
-
-      <v-btn
-        href="https://cpp.thankyou4caring.org/pages/studentclub"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Donate</span>
+        <v-btn  href="https://cpp.thankyou4caring.org/pages/studentclub"  target="_blank"  text>
+            <span>Donate</span>
+        </v-btn>
+        <br><br>
         
-      </v-btn>
-       <v-btn
-        href="https://cpp.thankyou4caring.org/pages/studentclub"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Contact</span>
-        
-      </v-btn>
-    </v-app-bar>
+        <v-btn  href="https://cpp.thankyou4caring.org/pages/studentclub"  target="_blank"  text>
+            <span>Contact</span>  
+        </v-btn>
+</v-navigation-drawer>
+
+</v-card>
 </template>
+
 <script>
-export default {
-  name: "Menu",
-  data() {
-    return {
-    };
-  },
+export default 
+{
+    name: "Menu",
+    data: () =>
+    ({
+        displayHamburger: false,
+        drawer: false,
+    }),
+  
+    methods:
+    {
+        checkBarWidth()
+        {
+            this.displayHamburger = window.innerWidth < 800;
+        }
+    },
+  
+    mounted()
+    {
+        window.addEventListener('resize', this.checkBarWidth);
+        this.checkBarWidth();
+    },
+    
+    beforeDestroy()
+    {
+        window.removeEventListener('resize', this.checkBarWidth);
+    },
 };
 </script>
+
 <style>
-.logo{
-  width: 13em;
-}
+    .logo{width: 13em;}
 </style>
