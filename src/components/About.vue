baaade07151c7190d4c08229cc0558fb8bc71021
kjkondo <44207296+kjkondo@users.noreply.github.com>
2020-08-04 22:22:45 -0700

    Supermemeageclub (#28)
    * instagram logo
    
    * fix insta link
    
    * mybar
    
    * Finished labling, check repeat sponsors
    
    * About us update
    
    Co-authored-by: Maximum Wilder-Smith <maxwildersmith@gmail.com>

--- src/components/About.vue
+++ src/components/About.vue
@@ -3,24 +3,47 @@
     <h2>
       About Us
     </h2>
+  <!--
     <p class="paragraph font-weight-regular">
-      The California Polytechnic University, Pomona SAE Supermileage club has two teams, engineering and computer science.
+      The California Polytechnic University, Pomona SAE Supermileage club has two teams, engineering and computer
+      science.
       The engineering team maintains and helps with the designs of the car, cfd, engineering reports for competition.
-      The computer science team works on the hardware for the autonomous systems, machine learning for navigation, and computer vision.
-      <br/>New members are always welcome, join below!
+      The computer science team works on the hardware for the autonomous systems, machine learning for navigation, and
+      computer vision.
+      <br />New members are always welcome, join below!
     </p>
-
-    <v-btn
-      href=""
-      target="_blank">
-        Engineering
+  -->
+    <p class="paragraph font-weight-regular">
+      <span class="tab">CPP SAE Supermileage was founded to engineer the most fuel</span>
+      efficient vehicles to fight against the climate change. Although it’s established in Fall
+      2018, the team built the Cal Poly Pomona’s first ever super fuel efficient
+      vehicle, Moonshot, from scratch in little bit over a year with no prior experience. CPP
+      SAE Supermileage designs, analyzes and manufactures its own aerodynamic shell,
+      chassis, steering system and power control units in house with funds from sponsors
+      and donors, almost entirely. In an attempt to create the ultimate fuel efficiency
+      machine, the team decided to embrace the latest developments in machine learning,
+      computer vision, sensor fusion and autonomous driving, and formed into an
+      engineering+computer science driven organization in early 2020.
+      <br>
+      <br>
+      <span class="tab">Although we are made up from engineering and computer science students,</span>
+      welcome any student who is very motivated to go the extra mile with us!
+    </P>
+    <v-btn href="" target="_blank">
+      Engineering
     </v-btn>
-    
-    <v-btn
-      href=""
-      target="_blank">
-        Computer Science        
+
+    <v-btn href="" target="_blank">
+      Computer Science
     </v-btn>
 
   </v-container>
 </template>
+<style scoped>
+.paragraph{
+  text-align: justify;
+}
+.tab{
+  margin-left:2em;
+}
+</style>
