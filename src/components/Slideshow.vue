1bb40bf247cb0db8dfd5d14d4f97cf1fd217aa42
The-Mills <mosonkahni@gmail.com>
2020-08-07 22:59:14 -0700

    Fixes Slideshow flickering and cleans up Slideshow code (#36)
    Co-authored-by: Daniel Milligan <ddmilligan@cpp.edu>

--- src/components/Slideshow.vue
+++ src/components/Slideshow.vue
@@ -1,47 +1,33 @@
 <template>
   <div>
-    <v-carousel
-      cycle
-      height="100%"
-      show-arrows-on-hover>
-        <v-carousel-item
-          v-for="(img, i) in imgs"
-          :key="i"
-          :src="img">
+    <v-carousel cycle height="100%" show-arrows-on-hover>
+        <v-carousel-item v-for="(img, i) in imgs" :key="i" eager>
+            <v-img height="100%" :src="img" eager/>
         </v-carousel-item>
     </v-carousel>
   </div>
 </template>
+
 <script>
 export default {
   name: "Slideshow",
-  props: {
-    // imgs: {
-    //   type: Array,
-    // },
-    delay: {
-      type: Number,
-      default: 4000,
-    }
-  },
-
-  data() {
-    return {
-      imgs:[]
-    };
-  },
-  created() {
+  
+  data: () => 
+  ({
+      imgs:[],
+  }),
+  
+  created() 
+  {
     this.importAll(require.context('@/assets/slideshow/', true, /\.(jpg|png|svg)$/))
   },
 
-  methods: {
-    importAll(r) {
+  methods: 
+  {
+    importAll(r) 
+    {
       r.keys().forEach(key =>(this.imgs.push({ src: r(key) })));
     }
   },
-  computed: {
-  }
 };
 </script>
-<style>
-</style>
